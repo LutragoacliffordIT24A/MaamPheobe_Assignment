bd9fd862401d532f4635d5596088240a00231e1b
LutragoacliffordIT24A <cliffordjohnburdeoslutrago@gmail.com>
2023-10-07 22:36:29 +0800

    Added initial files

---
+++ Employee.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+
+public class Employee {
+    private  String name;
+    private  double salary;
+    private  LocalDate hireDate;
+
+    public Employee(String name, double salary, LocalDate hireDate) {
+        this.name = name;
+        this.salary = salary;
+        this.hireDate = hireDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+}
